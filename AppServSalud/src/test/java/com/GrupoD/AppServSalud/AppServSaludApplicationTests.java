3835e0df49034aab605220caa4836a297bb0f93e
antoluduenabereziuk21 <antolube20@gmail.com>
2023-10-24 20:42:49 -0300

    feat:create base of proyect

---
+++ AppServSalud/src/test/java/com/GrupoD/AppServSalud/AppServSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrupoD.AppServSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
