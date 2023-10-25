e4b436ea8963071389b08e73679bb30dbd1692f3
j0k3r-PRG <j0k3r.rg2023@gmail.com>
2023-10-25 15:55:33 -0300

    creacion de la clase validacion y sus metodos

---
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/utilidades/Validacion.java
@@ -0,0 +1,14 @@
+package com.GrupoD.AppServSalud.utilidades;
+
+/**
+ * Validacion
+ *
+ * Clase que contiene metodos para validar datos ingresados por el usuario. Como por ejemplo
+ * si un String tiene solo letras, si no esta vacio o es null, que los numeros sean positivos,
+ * que un string solo contenga numeros, etc.
+ *
+ */
+public class Validacion {
+
+
+}
