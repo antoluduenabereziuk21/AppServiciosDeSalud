d801fc02cb196b73bdc0ec7e7ab70d2a36b7514e
unknown <ra31lopez@gmail.com>
2023-10-25 21:47:34 -0300

    Add: Creacion de Enum Rol y ObraSocial 2

--- AppServSalud/src/main/java/com/GrupoD/AppServSalud/utilidades/ObraSocialEnum.java
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/utilidades/ObraSocialEnum.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.GrupoD.AppServSalud.utilidades;
 
-/**
- *
- * @author Lenovo
- */
-public class ObraSocialEnum {
-    
+public enum ObraSocialEnum {
+    NoPosee,
+    OSECAC,
+    OSPE,
+    SwissMedical,
+    SancorSalud,
+    OSPRERA,
+    UPCN,
+    OSDE;
 }
