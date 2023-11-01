7d3be94a3cd705fc4c0b8da5b4c1942b858a13b4
unknown <ra31lopez@gmail.com>
2023-11-01 20:35:47 -0300

    Add: Entidad Profesional, entidad Oferta, varios enum relacionados y Repositorio Oferta

--- AppServSalud/src/main/java/com/GrupoD/AppServSalud/dominio/entidades/Profesional.java
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/dominio/entidades/Profesional.java
@@ -1,13 +1,20 @@
 package com.GrupoD.AppServSalud.dominio.entidades;
 
+import com.GrupoD.AppServSalud.utilidades.EspecialidadEnum;
+import com.GrupoD.AppServSalud.utilidades.ObraSocialEnum;
+import java.util.List;
+import javax.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Data
@@ -21,4 +28,17 @@ public class Profesional {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name="uuid", strategy = "uuid2")
     private String id;
+    
+    @Column(unique = true)
+    private String mp;
+    
+    @Enumerated(EnumType.STRING)
+    private EspecialidadEnum especialidad;
+    
+    private String descripcion;
+    
+    //private Oferta oferta;
+    
+    @Enumerated(EnumType.STRING)
+    private ObraSocialEnum obrasSociales;
 }
---
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/utilidades/HorarioEnum.java
@@ -0,0 +1,15 @@
+package com.GrupoD.AppServSalud.utilidades;
+
+public enum HorarioEnum {
+    _08HS,
+    _09HS,
+    _10HS,
+    _11HS,
+    _12HS,
+    _13HS,
+    _16HS,
+    _17HS,
+    _18HS,
+    _19HS,
+    _20HS;
+}
