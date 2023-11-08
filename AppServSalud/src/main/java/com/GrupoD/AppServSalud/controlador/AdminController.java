cd1ebe336a1da117a1947fe50428dbd9240bb8ae
Razier31 <ra31lopez@gmail.com>
2023-11-08 09:51:55 -0300

    Add: template editarAdmin configuracion controlador

--- AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
@@ -1,8 +1,11 @@
 package com.GrupoD.AppServSalud.controlador;
 
+import com.GrupoD.AppServSalud.dominio.entidades.Admin;
+import com.GrupoD.AppServSalud.dominio.entidades.Paciente;
 import com.GrupoD.AppServSalud.dominio.entidades.Usuario;
 import com.GrupoD.AppServSalud.dominio.servicios.AdminServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +22,18 @@ public class AdminController {
     private AdminServicio adminServicio;
 
     @GetMapping("/perfil/{email}")
-    public String perfil(@PathVariable String email, ModelMap modelo, HttpSession session){
+    public String perfil(@PathVariable String email, ModelMap modelo, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
         modelo.addAttribute("usuario", adminServicio.buscarPorEmail(usuario.getEmail()));
         return "vistaPerfil.html";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/modificar/{email}")
+    public String vistaModificarPaciente(@PathVariable String email, ModelMap modelo, HttpSession session) {
+        Admin admin = adminServicio.buscarPorEmail(email);
+        modelo.put("admin", admin);
+        return "forms/editarAdmin.html";
     }
 
 }
