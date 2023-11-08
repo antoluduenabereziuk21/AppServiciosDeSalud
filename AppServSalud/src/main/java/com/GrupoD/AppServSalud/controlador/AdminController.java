4de2f8b0bec566e9c4248aa8568dde5a3bb5da02
Razier31 <ra31lopez@gmail.com>
2023-11-08 19:23:59 -0300

    Add: Dos templates extras, modificando el controlador de Admin

--- AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
@@ -2,8 +2,12 @@ package com.GrupoD.AppServSalud.controlador;
 
 import com.GrupoD.AppServSalud.dominio.entidades.Admin;
 import com.GrupoD.AppServSalud.dominio.entidades.Paciente;
+import com.GrupoD.AppServSalud.dominio.entidades.Profesional;
 import com.GrupoD.AppServSalud.dominio.entidades.Usuario;
 import com.GrupoD.AppServSalud.dominio.servicios.AdminServicio;
+import com.GrupoD.AppServSalud.dominio.servicios.ProfesionalServicio;
+import com.GrupoD.AppServSalud.dominio.servicios.ServicioPaciente;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -21,6 +25,12 @@ public class AdminController {
     @Autowired
     private AdminServicio adminServicio;
 
+    @Autowired
+    private ServicioPaciente pacienteServicio;
+
+    @Autowired
+    private ProfesionalServicio profesionalServicio;
+
     @GetMapping("/perfil/{email}")
     public String perfil(@PathVariable String email, ModelMap modelo, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
@@ -30,10 +40,26 @@ public class AdminController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/modificar/{email}")
-    public String vistaModificarPaciente(@PathVariable String email, ModelMap modelo, HttpSession session) {
+    public String vistaModificarAdmin(@PathVariable String email, ModelMap modelo, HttpSession session) {
         Admin admin = adminServicio.buscarPorEmail(email);
         modelo.put("admin", admin);
         return "forms/editarAdmin.html";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/modPaciente/{email}")
+    public String vistaModificarPaciente(@PathVariable String email, ModelMap modelo, HttpSession session) {
+        Paciente paciente = pacienteServicio.buscarPorEmail(email);
+        modelo.put("paciente", paciente);
+        return "forms/editarPacienteAdmin.html";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/modProfesional/{email}")
+    public String vistaModificarProfesional(@PathVariable String email, ModelMap modelo, HttpSession session) {
+        Profesional profesional = profesionalServicio.buscarPorEmail(email);
+        modelo.put("profesional", profesional);
+        return "forms/editarProfesionalAdmin.html";
     }
 
 }
