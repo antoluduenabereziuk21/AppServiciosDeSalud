29e7964ef7b75e31b2ac6f064369b2f8b7d3eabf
j0k3r-rg <pdasriogallegos@gmail.com>
2023-11-17 13:35:41 -0300

    minimas correccciones

--- AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
+++ AppServSalud/src/main/java/com/GrupoD/AppServSalud/controlador/AdminController.java
@@ -102,7 +102,7 @@ public class AdminController {
     
     try {
       adminServicio.modificarAdmin(emailPath, email, nombre, apellido, dni, sexo, telefono);
-      return "redirect:/admin/perfil/" + email+"?exito=Administrador modificado correctamente";
+      return "redirect:/perfil/" + email+"?exito=Administrador modificado correctamente";
     } catch (MiExcepcion e) {
       modelo.put("error", e.getMessage());
       modelo.put("admin", adminServicio.buscarPorEmail(emailPath));
